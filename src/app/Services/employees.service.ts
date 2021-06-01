8ae56b3d3089dbc392f6ded68583a17d6e501856
Jamal40 <71548798+Jamal40@users.noreply.github.com>
2021-06-01 16:48:30 +0200

    Authentication EMp Service

--- src/app/Services/employees.service.ts
+++ src/app/Services/employees.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -8,33 +8,76 @@ export class EmployeesService {
   constructor(private http: HttpClient) {}
 
   public getAllEmployees() {
-    return this.http.get('https://localhost:44396/api/Employees');
+    let auth_token = sessionStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${auth_token}`,
+    });
+
+    return this.http.get('https://localhost:44396/api/Employees', {
+      headers: headers,
+    });
   }
 
   public getEmployeebyId(id) {
-    return this.http.get('https://localhost:44396/api/Employees/' + id);
+    let auth_token = sessionStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${auth_token}`,
+    });
+    return this.http.get('https://localhost:44396/api/Employees/' + id, {
+      headers: headers,
+    });
   }
 
   public addEmployee(fname, lname, salary, deptNo) {
-    return this.http.post('https://localhost:44396/api/Employees', {
-      fname: fname,
-      lname: lname,
-      salary: salary,
-      deptNo: deptNo,
+    let auth_token = sessionStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${auth_token}`,
     });
+
+    return this.http.post(
+      'https://localhost:44396/api/Employees',
+      {
+        fname: fname,
+        lname: lname,
+        salary: salary,
+        deptNo: deptNo,
+      },
+      {
+        headers: headers,
+      }
+    );
   }
 
   public editEmployee(empNo, fname, lname, salary, deptNo) {
-    return this.http.put('https://localhost:44396/api/Employees/' + empNo, {
-      empNo: empNo,
-      fname: fname,
-      lname: lname,
-      salary: salary,
-      deptNo: deptNo,
+    let auth_token = sessionStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${auth_token}`,
     });
+    return this.http.put(
+      'https://localhost:44396/api/Employees/' + empNo,
+      {
+        empNo: empNo,
+        fname: fname,
+        lname: lname,
+        salary: salary,
+        deptNo: deptNo,
+      },
+      { headers: headers }
+    );
   }
 
   public deleteEmployee(id) {
-    return this.http.delete('https://localhost:44396/api/Employees/' + id);
+    let auth_token = sessionStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${auth_token}`,
+    });
+    return this.http.delete('https://localhost:44396/api/Employees/' + id, {
+      headers: headers,
+    });
   }
 }
